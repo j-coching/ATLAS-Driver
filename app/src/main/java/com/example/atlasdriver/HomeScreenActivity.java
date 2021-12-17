00dcb7fa13a8a7c41645bf6c4a74be93f9ce36af
adrianpe35 <adrian_jenssen_pe@dlsu.edu.ph>
2021-12-17 18:51:31 +0800

    with video feed and map view

--- app/src/main/java/com/example/atlasdriver/HomeScreenActivity.java
+++ app/src/main/java/com/example/atlasdriver/HomeScreenActivity.java
@@ -29,7 +29,7 @@ public class HomeScreenActivity extends AppCompatActivity {
                 if(interface_counter == false){
                     replaceFragment(new MapFragment());
                     interface_counter = true;
-                    
+
                 }
                 else{
                     replaceFragment(new CCTVFragment());
